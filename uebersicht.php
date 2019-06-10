75f0e1554f9a6bf898c57dd10b7ed58e3d080e4c
Jonas Kortum <jonaskortum@googlemail.com>
2019-06-10 19:33:13 +0200

    Änderung Tab-Titel

--- uebersicht.php
+++ uebersicht.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Grill - Homepage 1#</title>
+    <title>Rezeptobot - Übersicht</title>
 
 
     <!---Font Icon-->
