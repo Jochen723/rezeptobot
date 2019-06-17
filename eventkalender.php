b3513bb0ac3f0c5c8b2648e32f692eb5ef609b48
Jonas Kortum <jonaskortum@googlemail.com>
2019-06-17 21:38:44 +0200

    Eventkalender Funktionen zurückgesetzt

--- eventkalender.php
+++ eventkalender.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Grill - Homepage 1#</title>
+    <title>Rezeptobot - Kochübersicht</title>
     <!---Font Icon-->
     <link href="css/font-awesome.min.css" rel="stylesheet">
 
@@ -47,289 +47,16 @@
    <link href="css/font-awesome.min.css" rel="stylesheet">
 
    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
-	<script type = "text/javascript" language = "javascript">
-	function renderDate(date) {
-			var res = date.split("/");
-
-			return res[2] + '-' + res[0] + '-' + res[1];
-		}
-
-    $( document ).ready(function() {
-		$("#target").click(function(){
-
-			var color = '#00cc99';
-
-			var e = document.getElementById("selectArt");
-			var titel = e.options[e.selectedIndex].innerText;
-
-			if (null != document.getElementById("inputGericht")) {
-				titel = 'Alternativ: ' + document.getElementById("inputGericht").value;
-			}
-
-
-			if (document.getElementById("selectArt").value === 'ess') {
-				color = '#FFCC99';
-			} else if (document.getElementById("selectArt").value == 'aus') {
-				color = '#66CCFF';
-			} else {
-				color = '#FFFFCC';
-			}
-
-
-
-			var myObj = {
-		"datum": renderDate(document.getElementById('date').value),
-		"essen_id": 0,
-		"titel" : titel,
-		"farbe" : color
-        }
-
-
-			$('#calendar').fullCalendar('renderEvent', {
-                title: titel,
-                start: renderDate(document.getElementById('date').value),
-                allDay: true,
-				backgroundColor : color
-              });
-
-			$.ajax({
-  type: "POST",
-  data: {event: JSON.stringify(myObj)},
-  dataType: "json",
-  url: 'db/saveEvent.php',
-  success:function(data){
-  },
-  error: function (request, error) {
-    },
-});
-
-$('#exampleModal').modal('hide');
-		});
-
-		$('#exampleModal').on('shown.bs.modal', function () {
-
-
-			var elem = document.getElementById("selectArt");
-			elem.addEventListener("change", test);
-
-			function test() {
-
-
-				if (document.getElementById("selectArt").value == "son") {
-					if (null == document.getElementById("labelGericht")) {
-				        var elem2 = document.getElementById("beschreibungDiv");
-				        var label = document.createElement('label');
-				        label.setAttribute("id", "labelGericht");
-				        label.innerHTML  = "Titel des Rezepts";
-				        var input = document.createElement("input");
-                        input.type = "text";
-						input.setAttribute("id", "inputGericht");
-                        input.classList.add("form-control");
-                        elem2.appendChild(label);
-				        elem2.appendChild(input);
-				    }
-
-				} else {
-					if (null !== document.getElementById("labelGericht")) {
-					    document.getElementById("labelGericht").remove();
-						document.getElementById("inputGericht").remove();
-					}
-				}
-
-
-
-				}
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+   <script src="js/eventscript.js"></script>
+   <script type = "text/javascript" language = "javascript">
 
 
 
 
-
-
-
-		})
-
-		var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-
-if (dd < 10) {
-  dd = '0' + dd;
-}
-
-if (mm < 10) {
-  mm = '0' + mm;
-}
-
-today = mm + '/' + dd + '/' + yyyy;
-
-
-
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'custom1',
-                right: 'title'
-        },
-            defaultDate: yyyy + '-'+mm+'-'+dd,
-            navLinks: true,
-			height: 600,
-      locales: 'ar-dz',
-            editable: true,
-			eventDrop: function(event, delta, revertFunc) {
-
-			var myObj2 = {
-		"datum": event.start.format(),
-		"event_id": event.id,
-        }
-
-		$.ajax({
-  type: "POST",
-  data: {event: JSON.stringify(myObj2)},
-  dataType: "json",
-  url: 'db/updateEvent.php',
-  success:function(data){
-  },
-  error: function (error) {
-    },
-});
-
-
-
-  },
-            eventLimit: true,
-
-            customButtons: {
-                custom1: {
-                    text: 'Alternatives Gericht hinzufügen',
-                    click: function () {
-						$("#exampleModal").modal()
-                    }
-                }
-            },
-
-
-        });
-
-		$('body').on('click', 'button.fc-prev-button', function() {
-			var events = $('#calendar').fullCalendar('clientEvents');
-	if(events.length > 0)
-{
-for(var i in events)
-{
-$('#calendar').fullCalendar('removeEvents',events[i].id);
-}
-}
-$.ajax({
-            type:'GET',
-            url:'db/loadEvents.php',
-            dataType: "json",
-
-            success:function(data){
-				for (var i = 0; i < data.length; i++) {
-					$('#calendar').fullCalendar('renderEvent', {
-                        id : data[i].event_id,
-						title: data[i].titel,
-                        start: data[i].datum,
-                        allDay: true,
-						backgroundColor : data[i].farbe
-                    });
-				}
-            },
-			error: function (request, error) {
-
-    },
-        });
-});
-
-$('body').on('click', 'button.fc-next-button', function() {
-var events = $('#calendar').fullCalendar('clientEvents');
-	if(events.length > 0)
-{
-for(var i in events)
-{
-$('#calendar').fullCalendar('removeEvents',events[i].id);
-}
-}
-$.ajax({
-            type:'GET',
-            url:'db/loadEvents.php',
-            dataType: "json",
-
-            success:function(data){
-				for (var i = 0; i < data.length; i++) {
-					$('#calendar').fullCalendar('renderEvent', {
-						id : data[i].event_id,
-                        title: data[i].titel,
-                        start: data[i].datum,
-                        allDay: true,
-						backgroundColor : data[i].farbe
-                    });
-				}
-            },
-			error: function (request, error) {
-
-    },
-        });
-});
-$(".fc-today-button").click(function() {
-	var events = $('#calendar').fullCalendar('clientEvents');
-	if(events.length > 0)
-{
-for(var i in events)
-{
-$('#calendar').fullCalendar('removeEvents',events[i].id);
-}
-}
-$.ajax({
-            type:'GET',
-            url:'db/loadEvents.php',
-            dataType: "json",
-
-            success:function(data){
-				for (var i = 0; i < data.length; i++) {
-					$('#calendar').fullCalendar('renderEvent', {
-						id : data[i].event_id,
-                        title: data[i].titel,
-                        start: data[i].datum,
-                        allDay: true,
-						backgroundColor : data[i].farbe
-                    });
-				}
-            },
-			error: function (request, error) {
-        console.log(arguments);
-    },
-        });
-});
-
-		$.ajax({
-            type:'GET',
-            url:'db/loadEvents.php',
-            dataType: "json",
-
-            success:function(data){
-				for (var i = 0; i < data.length; i++) {
-					$('#calendar').fullCalendar('renderEvent', {
-						id : data[i].event_id,
-                        title: data[i].titel,
-                        start: data[i].datum,
-                        allDay: true,
-						backgroundColor : data[i].farbe
-                    });
-				}
-            },
-			error: function (request, error) {
-        console.log(arguments);
-    },
-        });
-
-
-    });
-
-    </script>
+  </script>
 	<script>
+
     $(document).ready(function(){
       var date_input=$('input[name="date"]'); //our date input has the name "date"
       var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
@@ -341,6 +68,7 @@ $.ajax({
       };
       date_input.datepicker(options);
     })
+
 
     </script>
 	</script>
@@ -367,33 +95,6 @@ $.ajax({
                     <li class="nav-item">
                         <a class="nav-link" href="uebersicht.php">Von A - Z</a>
                     </li>
-                    <!--
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Demo
-              </a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="homepage-1.html">Homepage-1</a>
-                            <a class="dropdown-item" href="homepage-2.html">Homepage-2</a>
-                            <a class="dropdown-item" href="homepage-3.html">Homepage-3</a>
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown active">
-                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Recipes
-              </a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="browse-recipes.html">Browse Recipes</a>
-                            <a class="dropdown-item" href="recipe-detail.html">Recipe Detail</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="uebersicht.php" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                von A - Z
-              </a>
-                    </li>
-                  -->
                     <li class="nav-item btn-submit-recipe">
                         <a class="nav-link" href="neuesrezept.php"><i class="fa fa-upload" aria-hidden="true"></i> neues Rezept</a>
                     </li>
