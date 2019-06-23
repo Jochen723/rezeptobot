78dd005be9f3adeb2bf68c9dd046fd2a22d3d803
Jonas Kortum <jonaskortum@googlemail.com>
2019-06-23 16:58:30 +0200

    Sichern von anderen Rezepten als Event

--- js/eventscript.js
+++ js/eventscript.js
@@ -11,83 +11,74 @@ var carName = "Volvo";
 var newDate = "";
 
 $( document ).ready(function() {
-    var ttt = [];
-    var event22 = 'test';
-
-    $.ajax({
-        type:'GET',
-        url:'db/loadEvents.php',
-        dataType: "json",
-
-        success:function(data){
-            for (var i = 0; i < data.length; i++) {
-                var tttt = {
-                    id: data[i].event_id,
-                    title : data[i].titel,
-                    start : data[i].datum,
-                    backgroundColor : data[i].farbe
-                };
-
-                ttt.push(tttt);
-            }
-
-            $('#calendar').fullCalendar({
-                events: ttt,
-                header: {
-                    left: 'prev,next today',
-                    center: 'custom1',
-                    right: 'title'
-                },
-                eventClick: function(info) {
-
-                  carName = info;
-
-                  document.getElementById('modalDate').value=renderDate2(info.start._i);
-                  document.getElementById('modalTitel').innerHTML = info.title;
-
-                  document.getElementById('modalDate').name = info.id;
-
-
-                  $("#changeModal").modal();
-  },
-                customButtons: {
-                    custom1: {
-                        text: 'Alternatives Gericht hinzufügen',
-                        click: function () {
-                            $("#exampleModal").modal()
-                        }
-                    }
-                },
-           });
-       },
-});
-
-
-$("#changeTarget").click(function(){
-
-  newDate = renderDate(document.getElementById('modalDate').value);
-  var myObj = {
-      "datum": newDate,
-      "event_id" : document.getElementById('modalDate').name
-  }
-
+  var ttt = [];
+  var event22 = 'test';
 
   $.ajax({
+    type:'GET',
+    url:'db/loadEvents.php',
+    dataType: "json",
+    success:function(data){
+      for (var i = 0; i < data.length; i++) {
+        var tttt = {
+          id: data[i].event_id,
+          title : data[i].titel,
+          start : data[i].datum,
+          backgroundColor : data[i].farbe
+        };
+
+        ttt.push(tttt);
+      }
+
+      $('#calendar').fullCalendar({
+        events: ttt,
+        header: {
+          left: 'prev,next today',
+          center: 'custom1',
+          right: 'title'
+        },
+        eventClick: function(info) {
+          carName = info;
+          document.getElementById('modalDate').value=renderDate2(info.start._i);
+          document.getElementById('modalTitel').innerHTML = info.title;
+          document.getElementById('modalDate').name = info.id;
+          $("#changeModal").modal();
+        },
+        customButtons: {
+          custom1: {
+            text: 'Alternatives Gericht hinzufügen',
+            click: function () {
+              $("#exampleModal").modal()
+            }
+          }
+        },
+      });
+    },
+  });
+
+  $("#changeTarget").click(function(){
+    newDate = renderDate(document.getElementById('modalDate').value);
+    var myObj = {
+      "datum": newDate,
+      "event_id" : document.getElementById('modalDate').name
+    }
+
+    $.ajax({
       type: "POST",
       data: {event: JSON.stringify(myObj)},
       dataType: "text",
       url: 'db/updateEvent.php',
       success: function(php_script_response){
         var ev = carName;
-        ev.start._i = newDate;
-        $('#calendar').fullCalendar( 'removeEvents', ev.id );
-        $('#calendar').fullCalendar('renderEvent', {
-            title: ev.title,
-            start: newDate,
-            allDay: true,
-            backgroundColor : ev.backgroundColor
-        });
-        $('#changeModal').modal('hide');
+      ev.start._i = newDate;
+      $('#calendar').fullCalendar( 'removeEvents', ev.id );
+      $('#calendar').fullCalendar('renderEvent', {
+        title: ev.title,
+        start: newDate,
+        allDay: true,
+        backgroundColor : ev.backgroundColor
+      });
+      $('#changeModal').modal('hide');
     }
   });
 });
@@ -112,7 +103,7 @@ $("#target").click(function(){
 
     var myObj = {
         "datum": renderDate(document.getElementById('date').value),
-        "essen_id": 0,
+        "rezept_id": 0,
         "titel" : titel,
         "farbe" : color
     }
