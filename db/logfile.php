d808a2e906cef0bff975ea31eba21a00802d8d7a
Jonas Kortum <jonaskortum@googlemail.com>
2020-12-11 21:33:54 +0100

    Initiale Erstellung Logfile

---
+++ db/logfile.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+  function createLog($msg){ //function parameters, two variables.
+      
+      
+      $log_msg = date('Y-m-d H:i:s') . ' ' . $msg;
+      
+      $log_filename = "log";
+      if (!file_exists($log_filename))
+      {
+          // create directory/folder uploads.
+          mkdir($log_filename, 0777, true);
+      }
+      $log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
+      file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);      
+  }
+?>
