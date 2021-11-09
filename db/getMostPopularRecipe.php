4b634f4e515fd2a116036f46e28c15b8b78a616f
Jonas Kortum <jonaskortum@googlemail.com>
2021-11-09 10:58:42 +0100

    nächste Arbeitsstandsicherung

--- db/getMostPopularRecipe.php
+++ db/getMostPopularRecipe.php
@@ -14,7 +14,7 @@ try {
         'user_id' => (int) $_SESSION['userid']
     ];
 
-    $statement = $conn->prepare("SELECT * FROM event WHERE user_id = :user_id");
+    $statement = $conn->prepare("SELECT event.rezept_id, rezept.titel, rezept.bildpfad FROM event INNER JOIN rezept ON event.rezept_id = rezept.id WHERE event.user_id = :user_id");
     if ($statement->execute($data)) {
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))  {
             array_push($response, $row);
@@ -33,7 +33,59 @@ try {
             }
         }
 
-        $array = json_decode(json_encode($count_array), true);
+        class Motorcycle
+{
+    public $rezept_id;
+    public $anzahl;
+    public $titel;
+    public $bildpfad;
+}
+
+$bikes = array();
+
+foreach ($count_array as $rezept) {
+  $bike1 = new Motorcycle();
+  $bike1->rezept_id = $rezept['rezept_id'];
+  $bike1->anzahl = $rezept;
+  $bike1->titel = 'Titel 1';
+  $bike1->bildpfad = 'images/526586-420x280-fix-brokkoli-schinken-quiche.jpeg';
+
+  array_push($bikes, $bike1);
+
+  // code...
+}
+
+
+for ($i=0; $i < count($count_array); $i++) {
+
+
+
+
+
+}
+
+/*
+$bike1 = new Motorcycle();
+$bike1->rezept_id = 60;
+$bike1->anzahl = 2;
+$bike1->titel = 'Titel 1';
+$bike1->bildpfad = 'images/526586-420x280-fix-brokkoli-schinken-quiche.jpeg';
+$bike2 = new Motorcycle();
+$bike2->rezept_id = 62;
+$bike2->anzahl = 4;
+$bike2->titel = 'Titel 2';
+$bike2->bildpfad = 'images/526586-420x280-fix-brokkoli-schinken-quiche.jpeg';
+$bike3 = new Motorcycle();
+$bike3->rezept_id = 64;
+$bike3->anzahl = 3;
+$bike3->titel = 'Titel 3';
+$bike3->bildpfad = 'images/526586-420x280-fix-brokkoli-schinken-quiche.jpeg';
+
+
+$bikes = array($bike1, $bike2, $bike3);
+*/
+usort($bikes, "cmp");
+
 
     } else {
         throw new Exception($statement->errorInfo());
@@ -41,7 +93,7 @@ try {
 
     echo json_encode(array(
         'success' => true,
-        'response' => $array
+        'response' => $bikes
     ));
 
 } catch (Exception $ex){
@@ -50,4 +102,10 @@ try {
         'reason'  => $ex->getMessage(),
     ));
 }
+
+function cmp($a, $b) {
+    return strcmp($b->anzahl, $a->anzahl);
+}
+
+
 ?>
