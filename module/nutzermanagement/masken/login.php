1dc3703f3c6e76c55b274d164f6a4208cc8d21ae
Jochen723 <jonaskortum@googlemail.com>
2023-02-28 22:29:44 +0100

    Überarbeitung der Login-Maske

--- module/nutzermanagement/masken/login.php
+++ module/nutzermanagement/masken/login.php
@@ -32,49 +32,50 @@ session_destroy();
         <script type='text/javascript' src='../../../js/fullcalendar.js'></script>
         <link href="../css/login.css" rel="stylesheet">
         <script src="../js/login.js"></script>
-
-
-
     </head>
-
-
 
     <body>
 
-        <!-- Featured Recipes-->
-        <div class="featured">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-12">
-                        <img id="recipe_image" src="../../../logo/rezeptobot.png" alt="">
-                        <h3>Rezeptobot</h3>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="box grid recipes">
-                            <div class="by" style="text-align: center;"><i class="fa" aria-hidden="true"></i> Login</div>
-                            <div class="content">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-lg-12">
+    <div id="overlay">
+        <div class="cv-spinner">
+            <span class="spinner"></span>
+        </div>
+    </div>
 
-                                            <div class="form-group">
-                                                <label style="margin-top: 10px;">E-Mail</label>
-                                                <input id="mail" type="text" class="form-control">
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Passwort</label>
-                                                <input id="passwort" type="password" class="form-control">
-                                            </div>
+    <!-- Featured Recipes-->
+    <div class="featured">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <img id="recipe_image" src="../../../logo/rezeptobot.png" alt="">
+                    <h3>Rezeptobot</h3>
+                </div>
+                <div class="col-lg-8">
+                    <div class="box grid recipes">
+                        <div class="by" style="text-align: center;"><i class="fa" aria-hidden="true"></i> Login</div>
+                        <div class="content">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label style="margin-top: 10px;">E-Mail</label>
+                                            <input id="mail" type="text" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Passwort</label>
+                                            <input id="passwort" type="password" class="form-control">
+                                        </div>
 
-                                            <a id="rezeptaendern" class="btn btn-login" style="
-                                                background-color: #363636;
-                                                color: #fff;"> Login
-                                            </a>
+                                        <a id="rezeptaendern" class="btn btn-login" style="
+                                            background-color: #363636;
+                                            color: #fff;"> Login
+                                        </a>
                                             <!--<a id="rezeptaendern" href="registrierung.php" class="btn" style="
                                                 background-color: #363636;
                                                 color: #fff;"> Registrieren
                                             </a>-->
-                                        </div>
+                                        <br/>
+                                        <p id="loginFailed" style="color:red; display:none"> Benutzername oder Passwort falsch. </p>
                                     </div>
                                 </div>
                             </div>
@@ -83,5 +84,6 @@ session_destroy();
                 </div>
             </div>
         </div>
+    </div>
   </body>
 </html>
